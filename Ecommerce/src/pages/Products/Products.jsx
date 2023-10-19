81f14d15e66e6cca32e636e40096eca653f02cf4
Krishan Kumar Safi <142893219+krishukr01@users.noreply.github.com>
2023-10-19 18:03:39 +0530

    Product page fun : addition

--- Ecommerce/src/pages/Products/Products.jsx
+++ Ecommerce/src/pages/Products/Products.jsx
@@ -1,6 +1,11 @@
+import useFetch from "../../hooks/useFetch";
 import styles from "./Product.module.css";
 
 const Products = () => {
+  const [data, isLoading, isError] = useFetch(
+    " http://localhost:8080/MensData"
+  );
+
   return (
     <div className={styles.main_container}>
       <div className={styles.filter_container}></div>
