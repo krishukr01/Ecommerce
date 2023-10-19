1f51fef5a22a3b389f833249d778f1e1fc3e7686
Krishan Kumar Safi <142893219+krishukr01@users.noreply.github.com>
2023-10-19 11:12:45 +0530

    Navbar UI : modification

--- Ecommerce/src/components/Navbar/Navbar.jsx
+++ Ecommerce/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,23 @@
-import style from "./Navbar.module.css";
+import { Link } from "react-router-dom";
+
+import styles from "./Navbar.module.css";
+
 const Navbar = () => {
-  return <div className={style.nav_container}>Navbar</div>;
+  return (
+    <div className={styles.navContainer}>
+      <div className={styles.logo_container}>
+        <Link to="/">Ecommerce</Link>
+      </div>
+
+      <div className={styles.link_container}>
+        <Link to="/products">Products</Link>
+      </div>
+
+      <div className={styles.cart_container}>
+        <Link to="/cart"> {/* <CartIcon itemCount={cart.length} /> */}</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
